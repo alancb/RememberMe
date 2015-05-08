476e212007ac92c69d274bd08da408ff03febffa
Alan Barth <alancbarth@gmail.com>
2015-05-08 15:04:39 -0600

    Initial Commit

---
+++ RememberMe/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RememberMe
+//
+//  Created by Alan Barth on 5/8/15.
+//  Copyright (c) 2015 Alan Barth. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RememberMe/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  RememberMe
+//
+//  Created by Alan Barth on 5/8/15.
+//  Copyright (c) 2015 Alan Barth. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
